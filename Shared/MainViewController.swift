107fa3f01ac3fefeb875ac3cbfe7a10f142ec2b6
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-05-27 19:59:32 -0400

    Add login status text view

--- Shared/MainViewController.swift
+++ Shared/MainViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet weak var loginStatusTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateLoginStatus()
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +23,16 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func updateLoginStatus(){
+        FlickrKit.shared().checkAuthorization { (username, auth, name, error) in
+            DispatchQueue.main.async {
+                if (error != nil){
+                    self.loginStatusTextView.text = error?.localizedDescription
+                }
+                self.loginStatusTextView.text = "Logged in on Flickr with username \(username!) as \(name!)"
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
